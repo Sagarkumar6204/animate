ab883aa4ed9ea22aed63386eb523d2f5527b554c
Sagarkumar6204 <bittukumar9006716@gmail.com>
2026-03-10 15:36:00 +0530

    locomotive

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
-gsap.to("#page1 #box",{
-    scale: 0,
-    rotate: 360,
-    duration: 1,
-    delay: 1,
-    scrollTrigger: {
-        trigger: "#page1 #box",
-       scroller:'body',
-       start: "top 50%",
-       end:"top 30%",
-       scrub:5
-
-    }
-})
-gsap.to("#page2 #box",{
-    scale: 0,
-    rotate: -360,
-    duration: 1,
-    delay: 1,
-    scrollTrigger: {
-        trigger: "#page2",
-          scroller:'body',
-       start: "top 50%",
-       end:"top 30%",
-       scrub:1
-        
-    }
-})
-gsap.to("#page3 #box",{
-    scale: 0,
-    rotate: -360,
-    duration: 1,
-    delay: 1,
-    scrollTrigger: {
-        trigger: "#page3",
-        start: "top center",
-    }
+var scroll=new LocomotiveScroll({
+el:document.querySelector("#main"),
+smooth:true
 })
