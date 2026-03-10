a13d7c9dc34ab9f86a2b6b670c2871ba6ee395a5
Sagarkumar6204 <bittukumar9006716@gmail.com>
2026-03-10 14:53:46 +0530

    simple gsap

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.to('#circle',{
+    x:1200,
+    duration:2,
+    delay:1,
+    backgroundColor:'red',
+    rotate:360,
+    borderRadius:'10px',
+    scale:0.5,
+    repeat:-1,
+    yoyo:true
+})
+// gsap.from("h1",{
+//     y:50,
+//     duration:1,
+//     opacity:0,
+//     delay:1,
+//     stagger:1,
+// })
