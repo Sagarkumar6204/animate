e07adc4fd333c6d59db20de00593b121bd964765
Sagarkumar6204 <bittukumar9006716@gmail.com>
2026-03-10 15:21:27 +0530

    sroller  gsap

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-gsap.to('#circle',{
-    x:1200,
-    duration:2,
-    delay:1,
-    backgroundColor:'red',
-    rotate:360,
-    borderRadius:'10px',
-    scale:0.5,
-    repeat:-1,
-    yoyo:true
+gsap.to("#page1 #box",{
+    scale: 0,
+    rotate: 360,
+    duration: 1,
+    delay: 1,
+    scrollTrigger: {
+        trigger: "#page1 #box",
+       scroller:'body',
+       start: "top 50%",
+       end:"top 30%",
+       scrub:5
+
+    }
 })
-// gsap.from("h1",{
-//     y:50,
-//     duration:1,
-//     opacity:0,
-//     delay:1,
-//     stagger:1,
-// })
+gsap.to("#page2 #box",{
+    scale: 0,
+    rotate: -360,
+    duration: 1,
+    delay: 1,
+    scrollTrigger: {
+        trigger: "#page2",
+          scroller:'body',
+       start: "top 50%",
+       end:"top 30%",
+       scrub:1
+        
+    }
+})
+gsap.to("#page3 #box",{
+    scale: 0,
+    rotate: -360,
+    duration: 1,
+    delay: 1,
+    scrollTrigger: {
+        trigger: "#page3",
+        start: "top center",
+    }
+})
